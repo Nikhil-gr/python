c1063e5a4e83a74ac085d6ccf97a642b4a3883ea
Melo <razgiri111@gmail.com>
2026-06-06 19:26:01 +0545

    day1

---
+++ 1.py
@@ -0,0 +1,93 @@
+# number = int(input("Please type in a number: "))
+
+# if number >= 100:
+#     print("The number was greater than one hundred")
+#     number - 100
+#     print("Now its value has decreased by one hundred")
+#     print(f"Its value is now  {number}")
+    
+# print(f"{number} must be my lucky number!")
+# print("Have a nice day!")
+
+# user = float(input("floating number: "))
+# integer = int(user)
+# dec = user-integer
+# print("Integer part : ", integer)
+# print(f"Decimal Part: {dec:.2f}")
+
+# name = "Tim Tester"
+# age = 20
+# skill1 = "python"
+# level1 = "beginner"
+# skill2 = "java"
+# level2 = "veteran"
+# skill3 = "programming"
+# level3 = "semiprofessional"
+# lower = 2000
+# upper = 3000
+
+# print(f"my name is {name} I am {age} years old")
+# print("my skills are")
+# print(f"- {skill1} {level1}")
+# print(f"- {skill2} {level2}")
+# print(f"- {skill3} {level3}")
+# print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+# print(5, end="")
+# print(" + ", end="")
+# print(8,end="")
+# print(" - ",end="")
+# print(4,end="")
+# print(" = ",end="")
+# print(5 + 8 - 4)
+
+# user = int(input("input a number: "))
+# multiple = user * 5
+
+# print(f"{user} times 5 is {multiple}")
+
+# user = int(input("How many days? "))
+
+# seconds = 60 * 60 * 24
+
+# print("Seconds in that many days: ", user * seconds)
+
+# number1 = int(input("Number 1: "))
+# number2 = int(input("Number 2: "))
+# number3 = int(input("Number 3: "))
+# number4 = int(input("Number 4: "))
+
+
+# sum = number1 + number2 +number3 +number4
+# mean = sum / 4
+# print(f"The sum of the numbers is {sum} and the mean is {mean}")
+
+
+# user = float(input("How many times a week do you eat at the student cafeteria? "))
+# lunch = float(input("The price of a typical student lunch? "))
+# grocery = float(input("How much money do you spend on groceries in a week? "))
+
+# total = (user * lunch) + grocery
+# daily = total / 7
+
+# print("Average food expenditure:")
+# print("Daily:", daily)
+# print("Weekly:", total)
+
+# user = int(input("How many students on the course?"))
+# size = int(input("Desired group size?"))
+
+# groups = user //size
+
+# if user % size !=0:
+#     groups += 1
+
+# print("Number of groups formed: ", groups)
+
+
+
+
+
+
+
+  
