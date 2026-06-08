d54076698ba346e0eed7602cf77ab2f8a96c8e6b
Melo <razgiri111@gmail.com>
2026-06-08 15:47:40 +0545

    day 3 strings

---
+++ 3.py
@@ -0,0 +1,34 @@
+
+# test=1
+# user = int(input("Upper limit: "))
+# base= int(input("Base: "))
+
+# while test<=user :
+#     print(test)
+#     test*=base
+
+# test=1
+# user = int(input("Upper limit: "))
+
+# while test<=user :
+#     print(test)
+#     test*=2
+
+
+# limit = int(input("Limit: "))
+
+# total = 0
+# number = 1
+
+# while total<limit :
+#     total += number
+#     number += 1
+
+# print(total)
+
+
+# word = input("Please type in a string: ")
+# amt = int(input("Please type in an amount: "))
+# while amt>0:
+#     print(word, end="")
+#     amt-=1
