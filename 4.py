25c30f5431b56d77b63ca9bba528a0a1ba287877
Melo <razgiri111@gmail.com>
2026-06-09 20:46:27 +0545

    day 3

---
+++ 4.py
@@ -0,0 +1,16 @@
+# user = input("Please type in a string: ")
+# index = len(user)-1
+
+# while index >=0:
+#     print(user[index])
+#     index -= 1
+
+# width= int(input("Width: "))
+# while width>0:
+#     print("#", end="")
+#     width-=1
+
+# width= int(input("Width: "))
+# height = int(input("Height: "))
+
+# print(("#"*width+"\n")*height)
