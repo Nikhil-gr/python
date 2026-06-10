ab6cd0c8940ebad552f15da142330e7cd7b802ce
Melo <razgiri111@gmail.com>
2026-06-10 19:48:17 +0545

    strings

---
+++ 5.py
@@ -0,0 +1,48 @@
+# user = input("Please type in a string: ")
+
+# if len(user)<20:
+#     result=20-(len(user))
+#     print("*" * result + user)
+
+
+# user = input("Please type in a string: ")
+
+# i =1
+
+# while i<=len(user):
+#     print(user[:i])
+#     i+=1
+
+
+# vowels = "aeo"
+# while True:
+#     user = input("Please type in a string: ")
+    
+#     if user in vowels:
+#         print(f" found")
+#     else:
+#         print(f" not found")
+
+# input_string = "aeo"
+# i=0
+# substring = input("What are you looking for? ")
+
+# while i<len(input_string):
+    
+#     vowel = input_string[i]
+    
+#     if vowel in substring:
+#         print(f"{vowel} found")
+#     else:
+#         print(f"{vowel} not found")
+        
+#     i+=1
+
+
+
+       
+   
+    
+    
+    
+    
