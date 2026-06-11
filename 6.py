9f15f3d8f12dbf1908ffbbc6bc6dd73d8dfafa03
Melo <razgiri111@gmail.com>
2026-06-11 22:11:03 +0545

    functions

---
+++ 6.py
@@ -0,0 +1,66 @@
+# def seven_brothers():
+#     print("Aapo")    
+#     print("Eero")
+#     print("Juhani")
+#     print("Lauri")
+#     print("Simeoni")
+#     print("Timo")
+#     print("Tuomas")
+
+# if __name__ == "__main__":
+#     seven_brothers()
+
+# def first_character(text):
+#     print(text[0])
+
+# if __name__ == "__main__":
+#     first_character('python')
+#     first_character('yellow')
+#     first_character('tomorrow')
+#     first_character('heliotrope')
+#     first_character('open')
+#     first_character('night')
+
+# def mean(a, b,c):
+#     print((a+b+c)/3)
+# if __name__ == "__main__":
+#     mean(5, 3, 1)
+
+# def hash_square(length):
+#     count =0
+#     while count < length:
+#         print(("#"*length))
+#         count +=1
+
+# if __name__ == "__main__":
+#     hash_square(5)
+
+
+# def chessboard(length):
+#     count=0
+#     one = "10"*length
+#     zero = "01"*length
+    
+#     while count<length:
+#         if count % 2==0:
+#             print(one[:length])
+#         else:
+#             print(zero[:length])
+#         count +=1
+
+# if __name__ == "__main__":
+#     chessboard(3)
+
+# def squared(char, length):
+#     count=0
+#     one = char*length
+    
+#     while count<length:
+#         if count % 2==0:
+#             print(one[0:length])
+#         else:
+#             print(one[1])
+#         count +=1
+
+# if __name__ == "__main__":
+#     squared("abc", 5)
